6c9c3af2b56df22726b9f84522b1cc30b9e054bd
Jarppe <jarppe@metosin.fi>
2021-02-19 17:32:42 +0200

    Worm moving nicely

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
+let DEBUG: boolean = window.location.search.indexOf("debug") > 0
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
 
-const GAME_WIDTH  = 1000,
-      GAME_HEIGHT = GAME_WIDTH * 0.5625,
-      WORM_WIDTH  = 20,
-      HEADING     = [
+type Point = [number, number]
+
+
+type State = "run" | "paused" | "end"
+
+
+const GAME_WIDTH       = 1000,
+      GAME_HEIGHT      = GAME_WIDTH * 0.5625,
+      WORM_WIDTH       = 20,
+      HEADING: Point[] = [
         [0, -1], // Up
         [+1, 0], // Right
         [0, +1], // Down
@@ -19,41 +26,67 @@ const scaler = (fromMax: number, toMax: number) => {
 }
 
 
-type Point = [x: number, y: number]
-
-
 type Game = {
+  state: State,
   width: number,
   height: number,
+  wormWidth: number,
+  maxWormLength: number,
   scaleX: ReturnType<typeof scaler>,
   scaleY: ReturnType<typeof scaler>,
-  wormWidth: number,
-  wormLength: number,
-  head: Point
+  worm: Point[],
   heading: number,
-  joints: Point[],
 }
 
 
 const game: Game = {
-  width:      0,
-  height:     0,
-  scaleX:     scaler(0, 0),
-  scaleY:     scaler(0, 0),
-  wormWidth:  0,
-  wormLength: 100,
-  head:       [700, 50],
-  heading:    1,
-  joints:     [[700, 50]],
+  state:         "paused",
+  width:         0,
+  height:        0,
+  wormWidth:     0,
+  maxWormLength: 0,
+  scaleX:        scaler(0, 0),
+  scaleY:        scaler(0, 0),
+  worm:          [],
+  heading:       0,
+}
+
+
+const initGame = () => {
+  game.state = "run"
+  game.maxWormLength = 100
+  game.worm = [[200, 200], [200, 200]]
+  game.heading = 1
+}
+
+
+const drawDebugInfo = () => {
+  const debugData = [
+    `state:    ${ game.state }`,
+    ...(game.worm.map(([x, y], i) => `${ i === 0 ? "worm:    " : "         " } [${ i }] ${ x.toFixed(0) } : ${ y.toFixed(0) }`)),
+  ]
+
+  const lineHeight = 18
+  let y = 20
+
+  ctx.save()
+  ctx.font = "16px courier"
+  ctx.textBaseline = "hanging"
+  ctx.fillStyle = "rgba(128, 128, 128, 128)"
+
+  for (let line of debugData) {
+    ctx.fillText(line, 20, y)
+    y += lineHeight
+  }
+
+  ctx.restore()
 }
 
 
 const draw = () => {
   ctx.clearRect(0, 0, game.width, game.height)
-  ctx.save()
 
-  const joints = game.joints,
-        [x, y] = game.head
+  ctx.save()
 
   ctx.lineJoin = "round"
   ctx.lineCap = "round"
@@ -61,54 +94,64 @@ const draw = () => {
   ctx.strokeStyle = "#ff00ff"
 
   ctx.beginPath()
-  ctx.moveTo(game.scaleX(joints[0][0]), game.scaleY(joints[0][1]))
-  for (let i = 1; i < joints.length; i++) {
-    ctx.lineTo(game.scaleX(joints[i][0]), game.scaleY(joints[i][1]))
+  const worm = game.worm
+  const head = worm[0]
+  if (head) ctx.moveTo(game.scaleX(head[0]), game.scaleX(head[1]))
+  for (let i = 1; i < worm.length; i++) {
+    const [x, y] = worm[i]
+    ctx.lineTo(game.scaleX(x), game.scaleY(y))
   }
-  ctx.lineTo(game.scaleX(x), game.scaleY(y))
   ctx.stroke()
+
+  if (DEBUG) drawDebugInfo()
 
   ctx.restore()
 }
 
 
-const move = (ts: number): boolean => {
-  const [x, y]     = game.head,
-        [dx, dy]   = HEADING[game.heading],
-        [nx, ny]   = [x + dx, y + dy],
-        reach      = game.wormWidth * 0.4,
-        joints     = game.joints,
-        wormLength = game.wormLength
+const move = () => {
+  const worm     = game.worm,
+        head     = worm[0],
+        [dx, dy] = HEADING[game.heading],
+        [x, y]   = head,
+        reach    = game.wormWidth * 0.4,
+        maxLen   = game.maxWormLength
 
-  // let length     = 0,
-  //     jointCount = 0,
-  //     cx         = nx,
-  //     cy         = ny
-  //
-  // do {
-  //   let [x, y] = joints[jointCount]
-  //   length += Math.abs(cx - x) + Math.abs(cy - y)
-  //   cx = x
-  //   cy = y
-  //   jointCount += 1
-  // } while (length < wormLength && jointCount < joints.length)
-  //
-  // if (jointCount < joints.length) {
-  //   game.joints = joints.slice(0, jointCount - 1)
-  // }
+  let len = 0,
+      nx  = x + dx,
+      ny  = y + dy
 
-  game.head = [nx, ny]
-  return (nx >= reach)
-      && (nx <= GAME_WIDTH - reach)
-      && (ny >= reach)
-      && (ny <= GAME_HEIGHT - reach)
+  head[0] = nx
+  head[1] = ny
+
+  if ((nx < reach) || (nx > GAME_WIDTH - reach) || (ny < reach) || (ny > GAME_HEIGHT - reach)) {
+    game.state = "end"
+    return
+  }
+
+  for (let i = 1; i < worm.length; i++) {
+    const last   = worm[i],
+          [x, y] = last,
+          dx     = nx - x,
+          dy     = ny - y
+    len += Math.abs(dx) + Math.abs(dy)
+    if (len > maxLen) {
+      const over = maxLen - len
+      if (dx !== 0) last[0] += over * (dx > 0 ? -1 : +1)
+      if (dy !== 0) last[1] += over * (dy > 0 ? -1 : +1)
+      worm.splice(i + 1)
+      break
+    }
+    nx = x
+    ny = y
+  }
 }
 
 
-export const run = (ts: number) => {
-  const keepRunning = move(ts)
+export const run = () => {
+  if (game.state === "run") move()
   draw()
-  if (keepRunning) window.requestAnimationFrame(run)
+  window.requestAnimationFrame(run)
 }
 
 
@@ -125,7 +168,7 @@ const resize = () => {
   game.scaleY = scaler(GAME_HEIGHT, height)
   game.wormWidth = game.scaleX(WORM_WIDTH)
 
-  draw()
+  if (game.state !== "run") draw()
 }
 
 
@@ -134,20 +177,35 @@ resize()
 
 
 const turn = (direction: number) => {
-  const { head, heading, joints } = game,
-        nextHeading               = heading + direction
-  joints.push(head)
+  if (game.state !== "run") return
+  const heading     = game.heading,
+        nextHeading = heading + direction
   game.heading = (nextHeading < 0) ? 3 : (nextHeading > 3) ? 0 : nextHeading
+  game.worm.unshift([...game.worm[0]])
+}
+
+
+const action: { [state in State]: () => void } = {
+  "run":    () => game.state = "paused",
+  "paused": () => game.state = "run",
+  "end":    () => initGame(),
 }
 
 
 document.addEventListener("keydown", ({ code }) => {
   if (code === "ArrowRight") {
     turn(1)
-  } else if (code == "ArrowLeft") {
+  } else if (code === "ArrowLeft") {
     turn(-1)
+  } else if (code === "Space") {
+    action[game.state]()
+  } else if (code === "KeyD") {
+    DEBUG = !DEBUG
+  } else {
+    console.log(`key: ${ code }`)
   }
 })
 
 
-run(0)
+initGame()
+run()
